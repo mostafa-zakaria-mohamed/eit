c93c4531fa3b60702448c0f9c6c002c89db39b1b
MZ Digital Studio <mostafa.z.mohamed@gmail.com>
2024-08-04 21:56:46 +0300

    update-2

--- about.php
+++ about.php
@@ -29,7 +29,42 @@
         </div>
         <div class="row">
             <div class="col">
-                
+                <div class="wrapper">
+                    <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="mission-tab" data-bs-toggle="tab" data-bs-target="#mission-tab-pane" type="button" role="tab" aria-controls="mission-tab-pane" aria-selected="true">mission</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="vision-tab" data-bs-toggle="tab" data-bs-target="#vision-tab-pane" type="button" role="tab" aria-controls="vision-tab-pane" aria-selected="false">vision</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="values-tab" data-bs-toggle="tab" data-bs-target="#values-tab-pane" type="button" role="tab" aria-controls="values-tab-pane" aria-selected="false">values</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="mission-tab-pane" role="tabpanel" aria-labelledby="mission-tab" tabindex="0">
+                            <div class="content">
+                                <p>
+                                    A leading technology company in the field of electronic games, augmented reality, and virtual reality. We specialize in designing and developing interactive experiences and innovative solutions that meet customer needs across a multitude of sectors, including education, culture, tourism, gaming, and more
+                                </p>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="vision-tab-pane" role="tabpanel" aria-labelledby="vision-tab" tabindex="0">
+                            <div class="content">
+                                <p>
+                                    A leading technology company in the field of electronic games, augmented reality, and virtual reality. We specialize in designing and developing interactive experiences and innovative solutions that meet customer needs across a multitude of sectors, including education, culture, tourism, gaming, and more
+                                </p>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="values-tab-pane" role="tabpanel" aria-labelledby="values-tab" tabindex="0">
+                            <div class="content">
+                                <p>
+                                    A leading technology company in the field of electronic games, augmented reality, and virtual reality. We specialize in designing and developing interactive experiences and innovative solutions that meet customer needs across a multitude of sectors, including education, culture, tourism, gaming, and more
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
---
+++ inc/solution.php
@@ -0,0 +1,68 @@
+<section class="solutions">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="heading">
+                    <p class="pre-title">
+                        our solutions
+                    </p>
+                    <h2 class="title">
+                        Solutions
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Augmented Reality (AR) Applications
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <p>
+                                Augmented Reality (AR) technology is a counterpart of VR, distinguished by its ability to merge the real world with an interactive virtual one. Users interact with the enhanced environment through devices such as smartphones, tablets, and smart glasses. 
+                                </p>
+                                <p>
+                                AR has a variety of applications across a range of sectors (gaming, education, culture, heritage, training, tourism...). 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Virtual Reality (VR) Applications
+                        </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <p>
+                                Augmented Reality (AR) technology is a counterpart of VR, distinguished by its ability to merge the real world with an interactive virtual one. Users interact with the enhanced environment through devices such as smartphones, tablets, and smart glasses. 
+                                </p>
+                                <p>
+                                AR has a variety of applications across a range of sectors (gaming, education, culture, heritage, training, tourism...). 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Accordion Item #3
+                        </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
