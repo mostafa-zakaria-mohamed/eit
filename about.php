c28395d23fffeb53e06964c8e3e6aeef394c4c05
MZ Digital Studio <mostafa.z.mohamed@gmail.com>
2024-08-04 19:15:00 +0300

    update-1

--- about.php
+++ about.php
@@ -1,6 +1,38 @@
 <?php include('header.php'); ?>
 <?php include('./inc/top-page.php'); ?>
 
-
+<div id="about-page" class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-5 col-xl-5 col-lg-6 col-md-12">
+                <figure class="logo">
+                    <img src="./resources/theme/images/logo.png" alt="">
+                </figure>
+            </div>
+            <div class="col-xxl-7 col-xl-7 col-lg-6 col-md-12">
+                <div class="about-text">
+                    <h3>About EIT</h3>
+                    <p>
+                    Effective Impact Technology Company (EIT) strives to deliver comprehensive smart solutions for business development, tackling challenges across various fields.
+                    </p>
+                    <p>
+                    We offer consultancy services based on precise project needs analysis, outlining executable steps, and supervising implementation. 
+                    </p>
+                    <p>
+                    This includes establishing technical infrastructures, building electronic networks, and utilizing the latest suitable technologies for each project
+                    </p>
+                    <a href="#" class="btn btn-rose">
+                        Download Company Profile
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include('footer.php'); ?>
