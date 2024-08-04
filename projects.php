3683d80c9ed881ff3a2069bb32fe14dbf0eb8666
MZ Digital Studio <mostafa.z.mohamed@gmail.com>
2024-08-05 01:53:22 +0300

    update-3

--- projects.php
+++ projects.php
@@ -5,7 +5,42 @@
 <div id="projects-page" class="page">
     <div class="container">
         <div class="row">
-            
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12">
+                <div class="project-item">
+                    <figure>
+                        <img src="./resources/theme/images/project-thumb-1.png" alt="">
+                    </figure>
+                    <h5>
+                        <a href="#">
+                            project name here
+                        </a>
+                    </h5>
+                </div>
+            </div>
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12">
+                <div class="project-item">
+                    <figure>
+                        <img src="./resources/theme/images/project-thumb-2.png" alt="">
+                    </figure>
+                    <h5>
+                        <a href="#">
+                            project name here
+                        </a>
+                    </h5>
+                </div>
+            </div>
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12">
+                <div class="project-item">
+                    <figure>
+                        <img src="./resources/theme/images/project-thumb-1.png" alt="">
+                    </figure>
+                    <h5>
+                        <a href="#">
+                            project name here
+                        </a>
+                    </h5>
+                </div>
+            </div>
         </div>
     </div>
 </div>
