11137f95a6e7489d4f8e7f696a22db18787b18d4
MZ Digital Studio <mostafa.z.mohamed@gmail.com>
2024-08-04 23:43:18 +0300

    update-3

---
+++ inc/statistics.php
@@ -0,0 +1,43 @@
+<div class="statistics">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="heading">
+                    <h2 class="title">
+                        some facts
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="stats-wrapper">
+                    <div class="stats-item">
+                        <h3 class="counter" data-number="16">0</h3><span>K+</span>
+                        <p>
+                            Our Daily Game Users
+                        </p>
+                    </div>
+                    <div class="stats-item">
+                        <h3 class="counter" data-number="50">0</h3><span>M</span>
+                        <p>
+                            Game Downloads
+                        </p>
+                    </div>
+                    <div class="stats-item">
+                        <h3 class="counter" data-number="200">0</h3><span>+</span>
+                        <p>
+                            Game \ APP Launched
+                        </p>
+                    </div>
+                    <div class="stats-item">
+                        <h3 class="counter" data-number="36">0</h3><span>M+</span>
+                        <p>
+                            Project Delivered
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- projects.php
+++ projects.php
@@ -2,6 +2,14 @@
 
 <?php include('./inc/top-page.php'); ?>
 
+<div id="projects-page" class="page">
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+</div>
 
+<?php include('./inc/statistics.php'); ?>
 
 <?php include('footer.php'); ?>
