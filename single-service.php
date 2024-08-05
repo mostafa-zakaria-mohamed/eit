796db86f16750b5b1278fc997b2c5ef861a90316
MZ Digital Studio <mostafa.z.mohamed@gmail.com>
2024-08-05 03:06:17 +0300

    update-5

--- single-service.php
+++ single-service.php
@@ -2,6 +2,52 @@
 
 <?php include('./inc/top-page.php'); ?>
 
-
+<div id="single-service-page" class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <article class="content">
+                    <h1>Heading</h1>
+                    <h2>Heading</h2>
+                    <h3>Heading</h3>
+                    <h4>Heading</h4>
+                    <h5>Heading</h5>
+                    <h6>Heading</h6>
+                    <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                    </p>
+                    <p>
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                    </p>
+                    <p>
+                    Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                    </p>
+                    <ul>
+                        <li>
+                            <p>
+                            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam <a href="#">link here</a>
+                            </p>
+                        </li>
+                        <li>
+                            <a href="#">
+                            sed quia consequuntur magni dolores
+                            </a>
+                        </li>
+                        <li>
+                            <p>
+                            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam <a href="#">link here</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam <a href="#">link here</a>
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include('footer.php'); ?>
